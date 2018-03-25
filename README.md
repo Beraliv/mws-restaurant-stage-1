729f2e76f81b920f6006bc297f68b81a30f36efa
beraliv <beraliv.spb@gmail.com>
2018-03-25 19:31:16 +0300

    feat: add service worker
    update `README`, update `html`s, add `sw.js`, add icon, check offline availability

--- README.md
+++ README.md
@@ -45,10 +45,14 @@ With your server running, visit the site: [http://localhost:8000](http://localho
   - [x] Breadcrumbs have appropriate `aria` roles, structure and attributes (based on [W3 Breadcrumb Example](https://www.w3.org/TR/2017/NOTE-wai-aria-practices-1.1-20171214/examples/breadcrumb/index.html))
   - [x] Language is set to `html` as attribute `lang="en"`
   - [x] Main has a `main` role
-- [ ] Offline Availability
-  - [ ] Service worker is registered
-  - [ ] Service worker implements offline caching (in case of network failure)
+- [x] Offline Availability
+  - [x] Service worker is registered
+  - [x] Service worker implements offline caching (in case of network failure)
 - [x] General improvements
   - [x] JS style is set (using [ESLint](https://eslint.org/) based on [Udacity style guide](http://udacity.github.io/frontend-nanodegree-styleguide/javascript.html))
   - [x] CSS style is set (using [Style lint](https://stylelint.io/) based on [Udacity style guide](http://udacity.github.io/frontend-nanodegree-styleguide/css.html))
   - [x] Structure README (using [Udacity: Writing READMEs](https://classroom.udacity.com/courses/ud777))
+
+### License
+
+<div>Icon made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
---
+++ sw.js
@@ -0,0 +1,119 @@
+const startCacheName = 'restaurant-review-';
+const staticCacheName = 'restaurant-review-static-cache';
+const imagesCachesName = 'restaurant-review-images-cache';
+const allCaches = [
+  staticCacheName,
+  imagesCachesName
+];
+const filesToCache = [
+  // icon
+  'img/icon.ico',
+  // json
+  'data/restaurants.json',
+  // html
+  'index.html',
+  'restaurant.html',
+  // css
+  'css/responsive.css',
+  'css/styles.css',
+  // js
+  'js/dbhelper.js',
+  'js/main.js'
+];
+
+const isNotInCache = cacheName =>
+  cacheName.startsWith(startCacheName) && !allCaches.includes(cacheName);
+
+function registerHook() {
+  self.addEventListener('load', () => {
+    if ('serviceWorker' in navigator) {
+      console.warn('Service worker: Availability ✓');
+      navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.warn('Service worker: Register ✓');
+          console.warn(registration.scope);
+        })
+        .catch(error => {
+          console.warn('Service worker: Register ✗');
+          console.error(error);
+        });
+    } else {
+      console.error('Service worker: Availability ✗');
+    }
+  });
+}
+
+function installHook() {
+  self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open(staticCacheName).then(cache => cache.addAll(filesToCache))
+    );
+  });
+}
+
+function activateHook() {
+  self.addEventListener('activate', event => {
+    console.warn('Service worker: Activating... ✓');
+    event.waitUntil(
+      caches.keys().then(cacheNames => Promise.all(
+        cacheNames.filter(isNotInCache).map(cacheName => caches.delete(cacheName))
+      ))
+    );
+  });
+}
+
+function getOrFetchImage(request) {
+  const url = request.url;
+
+  return caches.open(imagesCachesName).then(cache => {
+    return cache.match(url).then(response => {
+      return response || fetch(request).then(networkResponse => {
+        cache.put(url, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
+
+function fetchHook() {
+  self.addEventListener('fetch', event => {
+    const url = new URL(event.request.url);
+
+    if (url.origin === location.origin) {
+      if (url.pathname === '/') {
+        console.warn('Service worker: Fetching homepage ✓', event.request.url);
+        event.respondWith(caches.match('index.html'));
+        return;
+      }
+      if (url.pathname.startsWith('/img/')) {
+        console.warn('Service worker: Fetching image ✓', event.request.url);
+        event.respondWith(getOrFetchImage(event.request));
+        return;
+      }
+    }
+
+    event.respondWith(
+      caches.match(event.request).then(response =>
+        response || fetch(event.request)
+      )
+    );
+  });
+}
+
+function messageHook() {
+  self.addEventListener('message', event => {
+    if (event.data.action === 'skipWaiting') {
+      self.skipWaiting();
+    }
+  });
+}
+
+function app() {
+  registerHook();
+  installHook();
+  activateHook();
+  fetchHook();
+  messageHook();
+}
+
+app();
